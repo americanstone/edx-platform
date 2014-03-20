c3ec99cc47b7dede7e49ea5b3fed65f8b46c813d
Alexander Kryklia <kryklia@gmail.com>
2014-03-20 17:18:38 +0200

    Split video player into smaller files.
    Add ugettext and fix typoes.

--- lms/djangoapps/courseware/features/video.py
+++ lms/djangoapps/courseware/features/video.py
@@ -48,7 +48,7 @@ coursenum = 'test_course'
 sequence = {}
 
 
-class ReuqestHandlerWithSessionId(object):
+class RequestHandlerWithSessionId(object):
     def get(self, url):
         """
         Sends a request.
@@ -448,7 +448,7 @@ def i_can_download_transcript(_step, format, text):
     }
 
     url = world.css_find(VIDEO_BUTTONS['download_transcript'])[0]['href']
-    request = ReuqestHandlerWithSessionId()
+    request = RequestHandlerWithSessionId()
     assert request.get(url).is_success()
     assert request.check_header('content-type', formats[format])
     assert (text.encode('utf-8') in request.content)
